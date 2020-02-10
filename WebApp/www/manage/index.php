be105ffe19dbfc5b579aa4951ee72557ab8326ab
François <francoislancian@gmail.com>
2020-02-10 12:09:27 +0100

    commit 10/02/2020 transfert de home vers cours

--- WebApp/www/manage/index.php
+++ WebApp/www/manage/index.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    require_once dirname(__DIR__, 2) . '/Loader.php';  //dirname — Renvoie le chemin du dossier parent
+    require_once dirname(__DIR__, 2) . '/Debug.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,16 +19,33 @@
     </header>
 
     <nav>
-        <ul><a href="">Profile</a></ul>
-        <ul><a href="">Quizzes</a></ul>
-        <ul><a href="">Categories</a></ul>
-        <ul><a href="">Questions</a></ul>
+        <ul class="menu">
+            <li><a href="?page=users">Profile</a></li>
+            <li><a href="?page=quizzes">Quizzes</a></li>
+            <li><a href="index.php?page=categories">Categories</a></li>
+            <li><a href="index.php?page=questions">Questions</a></li>
+        </ul>
     </nav>
 
     <main>
+        <?php
+
+        $page = !empty($_GET['page']) ? $_GET['page'] : 'home';
+        //$page = $_GET['page'] ?? 'home';      // null coalescing operator
+
+        $page = ($page . '.php');
+        //$page .= '.php';
+        echo $page;
+
+        if (is_file($page)) {  //verification de l'existance du fichier, le colle à cet endroit du code.
+            require $page;
+        } else {
+            echo 'message d\'erreur !';
+        }
+
+        ?>
 
     </main>
-
 </body>
 
 </html>
